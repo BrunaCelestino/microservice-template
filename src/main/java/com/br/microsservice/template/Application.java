198308e478a50189cf224743d3c5f80d946b63ae
BrunaMassuchini <brunaccelestino@gmail.com>
2023-02-25 16:42:33 -0300

    feat: initial setup

---
+++ src/main/java/com/br/microsservice/template/Application.java
@@ -0,0 +1,22 @@
+package com.br.microsservice.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/v1/microsservice")
+	@ResponseBody
+	String home(){
+		return "Microsservice-template 1.0";
+	}
+}
